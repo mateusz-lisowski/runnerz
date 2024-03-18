860e5b5b8c1d1798816859efd6993082abff2418
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-18 20:20:26 +0100

    Initial commit

---
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/SpringTutorialRunnerzApplication.java
@@ -0,0 +1,13 @@
+package com.github.mateuszlisowski.springtutorialrunnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialRunnerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialRunnerzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mateuszlisowski/springtutorialrunnerz/SpringTutorialRunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mateuszlisowski.springtutorialrunnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialRunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
