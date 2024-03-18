d5d8642dfe8e39654f2ba3738f34fe0597e6fc0f
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-18 20:20:57 +0100

    Rename main application entry point

--- src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/Application.java
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTutorialRunnerzApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTutorialRunnerzApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
