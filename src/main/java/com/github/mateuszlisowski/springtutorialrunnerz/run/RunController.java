26c857c1eadabd0e6ddc38202ec6205f6e8b0610
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-19 20:44:07 +0100

    Create endpoint for getting sing run by its id

--- src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/RunController.java
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/RunController.java
@@ -1,10 +1,14 @@
 package com.github.mateuszlisowski.springtutorialrunnerz.run;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -16,8 +20,18 @@ public class RunController {
         this.repository = repository;
     }
 
-    @GetMapping("run")
+    @GetMapping("")
     List<Run> getAllRuns() {
         return repository.getAllRuns();
     }
+
+    @GetMapping("/{id}")
+    Run getRunById(@PathVariable Integer id) {
+        Optional<Run> run = repository.getRunByID(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run with id " + id + " does not exist");
+        }
+        return run.get();
+    }
+
 }
