8b4877f0e3e79a6b8808d7c18e501576466c50ca
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-19 20:33:15 +0100

    Set new endpoint prefix

--- src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/RunController.java
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/RunController.java
@@ -1,22 +1,19 @@
 package com.github.mateuszlisowski.springtutorialrunnerz.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
     private final RunRepository repository;
 
     public RunController(RunRepository repository) {
         this.repository = repository;
-    }
-
-    @GetMapping("")
-    String root() {
-        return "root";
     }
 
     @GetMapping("run")
