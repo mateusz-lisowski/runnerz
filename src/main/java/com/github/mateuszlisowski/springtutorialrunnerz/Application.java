ac9ed9486004081b4960cf5ce90e4859d08fc68e
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-19 20:06:56 +0100

    Add test run object

--- src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/Application.java
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/Application.java
@@ -1,13 +1,31 @@
 package com.github.mateuszlisowski.springtutorialrunnerz;
 
+import com.github.mateuszlisowski.springtutorialrunnerz.run.Location;
+import com.github.mateuszlisowski.springtutorialrunnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "Morning swamp jogging", LocalDateTime.now(),
+					LocalDateTime.now().plusHours(1), 5, Location.OUTDOOR);
+			log.info("New run: " + run);
+		};
+	}
 }
