9f20ca5bc2fcf297a3f83073a51134b170a1ead7
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-21 22:59:15 +0100

    Implement run object validation

--- src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/Run.java
+++ src/main/java/com/github/mateuszlisowski/springtutorialrunnerz/run/Run.java
@@ -1,12 +1,23 @@
 package com.github.mateuszlisowski.springtutorialrunnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime finishedOn,
+        @Positive
         Integer kilometers,
         Location location
-) {}
+) {
+    public Run {
+        if(!finishedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Finished time must be after started time");
+        }
+    }
+}
